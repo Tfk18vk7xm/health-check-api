cfee306f4959bff640b8b96993d142fc0109da57
Lea Sony Jacob <lea.jacob@g10x.com>
2026-05-15 10:48:21 +0530

    Add gunicorn for deployment

--- app.py
+++ app.py
@@ -20,4 +20,24 @@ def time():
     }
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+
+
+# Installed & Verified git
+# Verified python installation
+# Create project folder
+# Initialized git repository
+# Created python virtual environment
+# Installed Flask
+# Created Flask application
+# Ran Flask application locally
+# Created .gitignore
+# Added fles to git
+# Checked git status
+# First commit
+# Created githyb repository
+# Connected local repo to github
+#created githb actions CI pipeline
+# Added CI pipeline
+# Triggered pipeline
+# Verified pipeline success
